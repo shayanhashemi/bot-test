2c6b908053488a799af8665b8c37ed0f22d49d46
shayan hashemi <shayanhashemi719@gmail.com>
2024-09-27 22:05:28 +0330

    firts try sending a message

--- test-bot.py
+++ test-bot.py
@@ -1,4 +1,11 @@
-print("test")
-import pyrogram
-print("test")
+from pyrogram import Client
 
+app = Client("my_account")
+
+
+async def main():
+    async with app:
+        await app.send_message("@shayann_hashemi", "Hi!")
+
+
+app.run(main())
